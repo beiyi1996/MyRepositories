8799181bbbc45560acef6b0125d124582e37f280
Winni Huang <usj0326@gmail.com>
2018-05-29 11:19:26 +0800

    更新自傳,滑動js檔案

---
+++ resume/js/slide.js
@@ -0,0 +1,12 @@
+$('#navcontact').click(function () {
+    $('html,body').animate({ scrollTop: $('#contact').offset().top }, 1000);
+});
+$('#navportfolio').click(function () {
+    $('html,body').animate({ scrollTop: $('#portfolio').offset().top }, 1000);
+});
+$('#navabout').click(function () {
+    $('html,body').animate({ scrollTop: $('#about').offset().top }, 1000);
+});
+$('#navhome').click(function () {
+    $('html,body').animate({ scrollTop: $('#home').offset().top }, 1000);
+});
