7a66ecf2395dc1c410adcd73dbbaef93893e66dd
unknown <winniwhuang@liontravel.com>
2020-02-11 18:02:55 +0800

    modified layout and style and delete style.css

--- resume/js/slide.js
+++ resume/js/slide.js
@@ -1,12 +1,24 @@
-$('#navcontact').click(function () {
-    $('html,body').animate({ scrollTop: $('#contact').offset().top }, 1000);
-});
-$('#navportfolio').click(function () {
-    $('html,body').animate({ scrollTop: $('#portfolio').offset().top }, 1000);
-});
-$('#navabout').click(function () {
-    $('html,body').animate({ scrollTop: $('#about').offset().top }, 1000);
-});
-$('#navhome').click(function () {
-    $('html,body').animate({ scrollTop: $('#home').offset().top }, 1000);
+$(function() {
+  console.log("ready!");
+
+  $("#navcontact").click(function() {
+    $("html,body").animate({ scrollTop: $("#contact").offset().top }, 1000);
+  });
+  $("#navportfolio").click(function() {
+    $("html,body").animate({ scrollTop: $("#portfolio").offset().top }, 1000);
+  });
+  $("#navabout").click(function() {
+    $("html,body").animate({ scrollTop: $("#about").offset().top }, 1000);
+  });
+  $("#navhome").click(function() {
+    $("html,body").animate({ scrollTop: $("#home").offset().top }, 1000);
+  });
+
+  $(".navIcon").on("click", function(event) {
+    console.log("menu icon is clicked!!", $(this), $(this).find(".mainNav"));
+    event.preventDefault();
+    $(this)
+      .siblings(".mainNav")
+      .toggleClass("slideDown");
+  });
 });
