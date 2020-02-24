b71d07f186b0ea3e77405ecc76b6d069155b1b85
Winni Huang <usj0326@gmail.com>
2020-02-24 13:46:54 +0800

    add scroll loading style and feature

--- resume/js/slide.js
+++ resume/js/slide.js
@@ -1,5 +1,15 @@
 $(function() {
   console.log("ready!");
+  $(window).scroll(function() {
+    console.log('window scroll top', $(this).scrollTop());
+    console.log('$("#collections")', $("#collections").offset().top);
+    var windowScrollTop = $(this).scrollTop();
+    var collectionsOffSetTop = $("#collections").offset().top;
+    if(windowScrollTop >= collectionsOffSetTop){
+      // console.log('要長出作品囉!!');
+      $(".timeline > li").addClass("scrollFade");
+    }
+  });
 
   $(".navIcon").on("click", function(event) {
     event.preventDefault();
